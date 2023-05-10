7fa3a7c755e9675f0d9c64a9e994b001221c86af
tzanchi <tzanchi@student.42berlin.de>
2023-05-10 17:08:01 +0200

    ft_lstadd_front correction

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:35:57 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/05/10 12:41:10 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/05/10 17:07:17 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (*lst)
-		new->next = *lst;
+	if (new == NULL)
+		return ;
+	new->next = *lst;
 	*lst = new;
 }
